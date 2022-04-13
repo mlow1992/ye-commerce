9a4fecf6e8f97ee71173d98cedec104fd8b06ba7
General Zelgius <mattlow123@gmail.com>
2022-04-13 14:28:50 -0400

    fixed table logic

--- models/index.js
+++ models/index.js
@@ -4,15 +4,14 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-Product.belongsTo(Category, {
-  foreignKey: 'category_id',
-  onDelete: 'CASCADE'
-});
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'id',
-  onDelete: 'CASCADE'
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL'
+});
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
 });
 
 // Products belongToMany Tags (through ProductTag)
